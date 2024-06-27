933df559cbaf832216104193f994d3b3d6dfe416
juangarciadelima <juangarciadelima884@gmail.com>
2024-06-27 11:33:23 -0300

    fix: correcting validation and messages on save user endpoint

--- src/routes/save-user.ts
+++ src/routes/save-user.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma.ts";
 import { UserType } from "@prisma/client";
 import { randomUUID } from "crypto";
-import { Elysia, t } from "elysia";
+import { Elysia, error, t } from "elysia";
 
 export const saveUser = new Elysia({
   tags: ["User"],
@@ -25,14 +25,18 @@ export const saveUser = new Elysia({
     });
 
     set.status = 200;
-    return { message: "Feedback created successfully" };
+    return { message: "User saved successfully" };
   },
   {
     body: t.Object({
       user: t.Object({
-        email: t.String({ minLength: 1 }),
+        email: t.String({
+          minLength: 1,
+          format: "email",
+          error: "Invalid Email",
+        }),
         name: t.String({ minLength: 1 }),
-        userType: t.Enum(UserType),
+        userType: t.Enum(UserType, { error: "Invalid User Type" }),
       }),
     }),
   }
