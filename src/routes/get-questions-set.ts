cfaeec76ed871d9ed8a9ad940b7c0adc9f1f3c49
juangarciadelima <juangarciadelima884@gmail.com>
2024-08-02 15:20:40 -0300

    fix: putting writable on questions set list response

--- src/routes/get-questions-set.ts
+++ src/routes/get-questions-set.ts
@@ -1,35 +1,36 @@
-import { Elysia } from "elysia"
-import { prisma } from "@/lib/prisma.ts"
+import { Elysia } from "elysia";
+import { prisma } from "@/lib/prisma.ts";
 
 export const getQuestions = new Elysia({
-	tags: ["Question Set"],
-	detail: {
-		description: "Get all the Questions Set",
-	},
+  tags: ["Question Set"],
+  detail: {
+    description: "Get all the Questions Set",
+  },
 }).get("/list-questions", async ({ set }) => {
-	const questions = await prisma.questionsSet.findMany({
-		orderBy: {
-			questionSetName: "asc",
-		},
-		select: {
-			id: true,
-			questionSetName: true,
-			activatedSet: true,
-			numberOfStars: true,
-			questions: {
-				select: {
-					questionName: true,
-					questionType: true,
-					questionDescription: true,
-				},
-			},
-		},
-	})
+  const questions = await prisma.questionsSet.findMany({
+    orderBy: {
+      questionSetName: "asc",
+    },
+    select: {
+      id: true,
+      questionSetName: true,
+      activatedSet: true,
+      numberOfStars: true,
+      writable: true,
+      questions: {
+        select: {
+          questionName: true,
+          questionType: true,
+          questionDescription: true,
+        },
+      },
+    },
+  });
 
-	if (!questions.length) {
-		set.status = 400
-		return { message: "Nenhum método de avaliação foi encontrado" }
-	}
+  if (!questions.length) {
+    set.status = 400;
+    return { message: "Nenhum método de avaliação foi encontrado" };
+  }
 
-	return { questions }
-})
+  return { questions };
+});
