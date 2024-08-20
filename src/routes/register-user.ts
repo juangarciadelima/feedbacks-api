4e4a0999721c032a20ba16b876400f0a0b32ec90
juangarciadelima <juangarciadelima884@gmail.com>
2024-08-20 18:09:32 -0300

    fix: adding validation to check if email exists on database when register

--- src/routes/register-user.ts
+++ src/routes/register-user.ts
@@ -1,35 +1,48 @@
-import { prisma } from '@/lib/prisma.ts';
-import { UserType } from '@prisma/client';
-import { Elysia, t } from 'elysia';
-
+import { prisma } from "@/lib/prisma.ts";
+import { UserType } from "@prisma/client";
+import { Elysia, t } from "elysia";
 
 export const registerUser = new Elysia({
   tags: ["User"],
-	detail: {
-		description: "Rgister user in database",
-	},
-}).post('/register', async ({body}) => {
-  const { user: userObject } = body
-
-  try {
-    await prisma.participants.create({
-      data: {
-        name: userObject.name,
-        email: userObject.email,
-        userType: UserType.PARTICIPANT,
-      },
-    })
-  } catch (err) {
-    console.log(err)
+  detail: {
+    description: "Rgister user in database"
   }
+}).post(
+  "/register",
+  async ({ body, set }) => {
+    const { user: userObject } = body;
 
-  return new Response(null, { status: 201 })
-}, {
-  body: t.Object({
-    user: t.Object({
-      name: t.String({ minLength: 1 }),
-      email: t.String({ minLength: 1, format: 'email' }),
-      password: t.String({ minLength: 1 }),
+    const existingUser = await prisma.participants.findFirst({
+      where: {
+        email: userObject.email
+      }
+    });
+
+    if (existingUser) {
+      set.status = 400;
+      throw new Error("E-mail já possui cadastro no sistema");
+    }
+    try {
+      await prisma.participants.create({
+        data: {
+          name: userObject.name,
+          email: userObject.email,
+          userType: UserType.PARTICIPANT
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    return new Response(null, { status: 201 });
+  },
+  {
+    body: t.Object({
+      user: t.Object({
+        name: t.String({ minLength: 1 }),
+        email: t.String({ minLength: 1, format: "email" }),
+        password: t.String({ minLength: 1 })
+      })
     })
-  })
-})
+  }
+);
